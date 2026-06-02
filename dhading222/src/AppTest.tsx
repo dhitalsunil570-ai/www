a2bb1245c9952beaf3c80f74591dc83742170273
v0agent <it+v0agent@vercel.com>
2026-06-02 20:49:41 +0000

    chore: remove unused modulepreload script and React files

---
+++ dhading222/src/AppTest.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Dhading Hospital</h1>
+      <p>This is a test to see if React is rendering</p>
+    </div>
+  );
+}
